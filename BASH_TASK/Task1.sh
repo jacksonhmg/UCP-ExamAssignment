32cdb9d30136b3455ac6f356cdb9363c3611caa0
jacksonhmg <jacksonmowattgok@gmail.com>
2022-11-04 16:05:55 +0800

    added to comment

--- BASH_TASK/Task1.sh
+++ BASH_TASK/Task1.sh
@@ -1,2 +1,2 @@
-# '$1 is command line input of logfile.txt'. The first half before '$1' is checking if the line has an entry after 08:53:00, the second half is checking if it has an IPV4 address. The structure of the '|' and multiple greps works as an AND condition
+# '$1' is third command line input of 'logfile.txt'. The first half before '$1' is checking if the line has an entry after 08:53:00, the second half is checking if it has an IPV4 address. The structure of the '|' and multiple greps works as an AND condition
 grep '08:5[3-4]:[0-5][0-9]' $1 | egrep '[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}\.[[:digit:]]{1,3}'
